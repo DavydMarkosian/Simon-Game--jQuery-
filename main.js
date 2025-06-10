b2742cc2c94598b77556fcb80035d610163824c4
David Markosian <markdaiv97@gmail.com>
2025-06-09 21:34:29 -0500

    Fn of lvls

--- main.js
+++ main.js
@@ -1,27 +1,29 @@
 let arrayOfRandoms = [];
 let clickedBlocks = [];
-let level;
+let lvl = 1;
 
 //================== start levels ===============
-
-for (let i = 0; i < 5; i++) {
-  setTimeout(function () {
-    let randomNumber = Math.floor(Math.random() * 4) + 1;
-
-    $("#" + randomNumber).animate({ opacity: 0.5 });
-    $("#" + randomNumber).animate({ opacity: 1 });
-    $("#" + randomNumber).addClass(" boxShadow transform");
-
+function startLvl() {
+  for (let i = 0; i < lvl; i++) {
     setTimeout(function () {
-      $("#" + randomNumber).removeClass(" boxShadow transform");
-    }, 300);
+      let randomNumber = Math.floor(Math.random() * 4) + 1;
 
-    arrayOfRandoms.push(randomNumber);
+      $("#" + randomNumber).animate({ opacity: 0.5 });
+      $("#" + randomNumber).animate({ opacity: 1 });
+      $("#" + randomNumber).addClass(" boxShadow transform");
 
-    console.log(arrayOfRandoms);
-    return arrayOfRandoms;
-  }, i * 800);
+      setTimeout(function () {
+        $("#" + randomNumber).removeClass(" boxShadow transform");
+      }, 300);
+
+      arrayOfRandoms.push(randomNumber);
+
+      console.log(arrayOfRandoms);
+      return arrayOfRandoms;
+    }, i * 800);
+  }
 }
+startLvl();
 
 //================= choose the block =========================
 $(".block").click(function (e) {
@@ -35,34 +37,55 @@ $(".block").click(function (e) {
     $(".block").removeClass(" boxShadow transform");
   }, 300);
 
-  //   console.log("arrayOfRandoms: " + arrayOfRandoms);
-  //   console.log(e.target);
-
   compareArrays(clickedBlocks, arrayOfRandoms, e.target);
 });
 
 //==================== conditions (Checking) ========================
 
 function compareArrays(clickedBlocks, arrayOfRandoms, target) {
-  for (let i = 0; i < arrayOfRandoms.length; i++) {
-    let rightBlock = arrayOfRandoms[i];
-    console.log(rightBlock);
+  console.log(`your choice: ${target.id}`);
 
-    if (rightBlock == target.id) {
-      console.log("good");
+  for (let i = 0; i < clickedBlocks.length; i++) {
+    switch (clickedBlocks[i] !== arrayOfRandoms[i]) {
+      case true:
+        console.log("you lose");
+        $(target).addClass("boxShadowRed transform");
+
+        setTimeout(function () {
+          $(target).removeClass(" boxShadowRed transform");
+        }, 200);
+        setTimeout(function () {
+          $(target).addClass(" boxShadowRed transform");
+        }, 300);
+
+        break;
+
+      case false:
+        $(target).addClass("boxShadowGreen transform");
+        console.log(clickedBlocks);
+
+        setTimeout(function () {
+          $(target).removeClass(" boxShadowGreen transform");
+        }, 300);
+
+        break;
+
+      default:
+        break;
     }
-
-    // if (arrayOfRandoms[i] != target.id) {
-    //   console.log(target.id);
-
-    //   console.log(arrayOfRandoms[i]);
-
-    //   console.log("Fail!");
-
-    //   $(target).addClass("boxShadowRed");
-    // }
-    // if (arrayOfRandoms[i] == target.id) {
-    //   $(target).addClass("boxShadowGreen");
-    // }
+  }
+  if (clickedBlocks.length === arrayOfRandoms.length) {
+    lvlCounter();
+    setTimeout(() => {
+      startLvl();
+    }, 2000);
   }
 }
+
+function lvlCounter() {
+  lvl = lvl + 1;
+  console.log("lvl: " + lvl);
+  $("h1").text(`Level ${lvl}`);
+  return lvl;
+}
+console.log(lvl);
